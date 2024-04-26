4ffb16bdc0dbac55f25bb2be11d27ecb2965cc16
Nikk Jackson <NikkolasJ@Outlook.com>
2024-04-26 15:27:25 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# CodingClicker
-An idle game on Coding.
+# coding_clicker
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/electron.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("node:url");
+const path = require("node:path");
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "Coding Clicker",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  window.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "../dist/index.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+
+  window.resizable = false;
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
